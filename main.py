ac2a29aea2201eab1f5af1cde9689aa39c5268f8
Njabulo Masilela <masilelanjabulo01@gmail.com>
2022-10-26 13:33:06 +0200

    view the dataset

--- main.py
+++ main.py
@@ -7,3 +7,7 @@ from statistics import median
 
 # read in the dataset
 df = pd.read_csv('Lightning_Strokes_JHB_20km.csv')
+
+# view the data in the dataset
+print('The dataset containts the following \n')
+print(df.head())
