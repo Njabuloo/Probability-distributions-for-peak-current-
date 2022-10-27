9020d017592a89bd2ee20ddcadb55bae6dd5ffac
Njabulo Masilela <masilelanjabulo01@gmail.com>
2022-10-27 02:58:34 +0200

    Histogram - distribution of the magnitude of the peak current

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import seaborn as sns
 from statistics import median
 
 # read in the dataset
-df = pd.read_csv('Lightning_Strokes_JHB_20km.csv')
+df = pd.read_csv('../Lightning_Strokes_JHB_20km.csv')
 
 # transform the peak currents to their magnitudes
 df['Peak Current Magnitude'] = abs(df['Peak Current'])
@@ -18,9 +18,21 @@ print(df.head())
 # description of the data
 print("The statistical description of the data is as follows :")
 print(df.describe())
+print('\n')
 
 # get the mean, median and mode
-mean = df['Peak Current'].mean()
-median = df['Peak Current'].median()
-mode = df['Peak Current'].mode()
-print(f'The mean is = {mean}, the median is = {median} and the mode is = {mode}.')
+mean = df['Peak Current Magnitude'].mean()
+median = df['Peak Current Magnitude'].median()
+mode = df['Peak Current Magnitude'].mode()
+sd = df['Peak Current Magnitude'].std()
+variance = df['Peak Current Magnitude'].var()
+print(f'The mean is = {mean}, the median is = {median}, the mode is = {mode} and the variance is {variance}.')
+print('\n')
+
+# view how the peak current is distributed
+plt.hist(df['Peak Current Magnitude'], color='blue',bins=500)
+plt.xlabel('Magnitude of the peak current')
+plt.ylabel('No of peak currents')
+plt.title('Magnitude peak current distribution')
+plt.grid(True)
+plt.savefig('Magnitude_peak_current.png')
