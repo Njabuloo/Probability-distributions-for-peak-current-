b1c7f8eb6c7472e162227a43513018470bf519a7
Njabulo Masilela <masilelanjabulo01@gmail.com>
2022-10-27 03:01:45 +0200

    Histogram - distribution of the magnitude of the peak current

--- main.py
+++ main.py
@@ -36,3 +36,21 @@ plt.ylabel('No of peak currents')
 plt.title('Magnitude peak current distribution')
 plt.grid(True)
 plt.savefig('Magnitude_peak_current.png')
+
+# distribution for positive values
+positive_currents = df[ df['Peak Current'] > 0 ]
+plt.hist(positive_currents['Peak Current'], color='blue',bins=500)
+plt.xlabel('Magnitude of the positive peak current')
+plt.ylabel('No of peak currents')
+plt.title('Magnitude of positive peak current distribution')
+plt.grid(True)
+plt.savefig('Magnitude_peak_current_positive_currents.png')
+
+# distribution for positive values
+negative_currents = df[ df['Peak Current'] < 0 ]
+plt.hist(abs(negative_currents['Peak Current']), color='blue',bins=500)
+plt.xlabel('Magnitude of the negative peak current')
+plt.ylabel('No of peak currents')
+plt.title('Magnitude of negative peak current distribution')
+plt.grid(True)
+plt.savefig('Magnitude_peak_current_negative_currents.png')
