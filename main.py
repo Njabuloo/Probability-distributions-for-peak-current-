9f299ea6847a50152d4bf63f179a5f48a314de8a
Njabulo Masilela <masilelanjabulo01@gmail.com>
2022-10-31 01:10:49 +0200

    refactoring

--- main.py
+++ main.py
@@ -30,73 +30,85 @@ def bestDistributions(data: list, title: str, choosen_distributions: list) -> No
     print("\n")
 
 
-# read in the dataset
-df = pd.read_csv("Lightning_Strokes_JHB_20km.csv")
+def data_exploration() -> pd.DataFrame:
+    """reads in the dataset and prints it's statistical descriptions"""
+    df = pd.read_csv("Lightning_Strokes_JHB_20km.csv")
+    # view the data in the dataset
+    print("The dataset containts the following \n")
+    print(df.head())
+    print("\n")
+    # description of the data
+    print("The statistical description of the data is as follows :")
+    print(df.describe())
+    print("\n")
+    # get the mean, median and mode
+    mean = df["Peak Current"].mean()
+    median = df["Peak Current"].median()
+    mode = df["Peak Current"].mode()
+    sd = df["Peak Current"].std()
+    variance = df["Peak Current"].var()
+    print(
+        f"The mean is = {mean}, the median is = {median}, the mode is = {mode} and the variance is {variance}."
+    )
+    print("\n")
+    return df
 
-# view the data in the dataset
-print("The dataset containts the following \n")
-print(df.head())
-print("\n")
 
-# description of the data
-print("The statistical description of the data is as follows :")
-print(df.describe())
-print("\n")
+def distributions(df: pd.DataFrame) -> None:
+    # view how the peak current is distributed
+    plot_hist(df["Peak Current"], 1000, "")
+    # distribution for positive values
+    positive_currents = df[df["Peak Current"] > 0]
+    plot_hist(positive_currents["Peak Current"], 100, "Positive")
+    # distribution for positive values
+    negative_currents = df[df["Peak Current"] < 0]
+    plot_hist(abs(negative_currents["Peak Current"]), 100, "Negative")
 
-# get the mean, median and mode
-mean = df["Peak Current"].mean()
-median = df["Peak Current"].median()
-mode = df["Peak Current"].mode()
-sd = df["Peak Current"].std()
-variance = df["Peak Current"].var()
-print(
-    f"The mean is = {mean}, the median is = {median}, the mode is = {mode} and the variance is {variance}."
-)
-print("\n")
 
-# view how the peak current is distributed
-plot_hist(df["Peak Current"], 1000, "")
+def tests_and_pdfs(df: pd.DataFrame) -> None:
+    # fitting distributions
+    peak_currents = df["Peak Current"]
+    choosen_distributions = ["norm", "cauchy", "logistic"]
+    bestDistributions(peak_currents, "", choosen_distributions)
+    # difference in distributions between the positive and negative
+    choosen_distributions = ["lognorm", "exponnorm", "weibull_max", "gamma", "alpha"]
+    # for the positive distribution
+    positive_currents = df[df["Peak Current"] > 0]
+    bestDistributions(
+        positive_currents["Peak Current"], "Magnitude", choosen_distributions
+    )
+    # for the negative distribution
+    negative_currents = df[df["Peak Current"] < 0]
+    bestDistributions(
+        abs(negative_currents["Peak Current"]), "negative", choosen_distributions
+    )
+    plt.close()
 
-# distribution for positive values
-positive_currents = df[df["Peak Current"] > 0]
-plot_hist(positive_currents["Peak Current"], 100, "Positive")
 
-# distribution for positive values
-negative_currents = df[df["Peak Current"] < 0]
-plot_hist(abs(negative_currents["Peak Current"]), 100, "Negative")
+def cdfs(df: pd.DataFrame) -> None:
+    # plotting the CDFs
+    peak_currents = df["Peak Current"]
+    sorted_peak_currents = np.sort(peak_currents)
+    normal_distribution_cdf = ss.norm.cdf(sorted_peak_currents)
+    cauchy_distribution_cdf = ss.cauchy.cdf(sorted_peak_currents)
+    logistic_distribution_cdf = ss.logistic.cdf(sorted_peak_currents)
+    data_points = len(sorted_peak_currents)
+    sorted_peak_currents_cdf = np.arange(data_points) / float(data_points)
+    plt.plot(sorted_peak_currents, sorted_peak_currents_cdf, label="Peak current")
+    plt.plot(sorted_peak_currents, normal_distribution_cdf, label="normal")
+    plt.plot(sorted_peak_currents, cauchy_distribution_cdf, label="cauchy")
+    plt.plot(sorted_peak_currents, logistic_distribution_cdf, label="logistic")
+    plt.grid(True)
+    plt.xlabel("Peak current (KA)")
+    plt.ylabel("Probability")
+    plt.title("Emphirical vs Theoretical CDFs plot")
+    plt.legend()
+    plt.savefig("CDFs.png")
+    plt.close()
 
-# fitting distributions
-peak_currents = df["Peak Current"]
-choosen_distributions = ["norm", "cauchy", "logistic"]
-bestDistributions(peak_currents, "", choosen_distributions)
 
-# difference in distributions between the positive and negative
-choosen_distributions = ["lognorm", "exponnorm", "weibull_max", "gamma", "alpha"]
-
-# for the positive distribution
-bestDistributions(positive_currents["Peak Current"], "Magnitude", choosen_distributions)
-
-# for the negative distribution
-bestDistributions(
-    abs(negative_currents["Peak Current"]), "negative", choosen_distributions
-)
-plt.close()
-
-# plotting the CDFs
-sorted_peak_currents = np.sort(peak_currents)
-normal_distribution_cdf = ss.norm.cdf(sorted_peak_currents)
-cauchy_distribution_cdf = ss.cauchy.cdf(sorted_peak_currents)
-logistic_distribution_cdf = ss.logistic.cdf(sorted_peak_currents)
-data_points = len(sorted_peak_currents)
-sorted_peak_currents_cdf = np.arange(data_points) / float(data_points)
-plt.plot(sorted_peak_currents, sorted_peak_currents_cdf, label="Peak current")
-plt.plot(sorted_peak_currents, normal_distribution_cdf, label="normal")
-plt.plot(sorted_peak_currents, cauchy_distribution_cdf, label="cauchy")
-plt.plot(sorted_peak_currents, logistic_distribution_cdf, label="logistic")
-plt.grid(True)
-plt.xlabel("Peak current (KA)")
-plt.ylabel("Probability")
-plt.title("Emphirical vs Theoretical CDFs plot")
-plt.legend()
-plt.savefig("CDFs.png")
-plt.close()
+if __name__ == "__main__":
+    df = data_exploration()
+    distributions(df)
+    tests_and_pdfs(df)
+    cdfs(df)
